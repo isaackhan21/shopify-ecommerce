f1f994a26bf634b333302e9b00b0233f349af0ee
Isaac Khan <isaackhan21@gmail.com>
2022-04-25 10:10:03 +0100

    Fetching Products with GraphQL

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
+import { getProductsInCollection } from "../lib/shopify";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ products }) {
+  console.log(products);
   return <div className="text-3xl">Hello Shopify Next.js</div>;
+}
+
+export async function getStaticProps(context) {
+  const products = await getProductsInCollection();
+  return {
+    props: { products },
+  };
 }
